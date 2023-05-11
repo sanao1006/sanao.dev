53b6527b3a6fc183ec281e36f2eee0192ce5e8ef
sanao1006 <naosak1006@gmail.com>
2023-05-11 15:58:18 +0900

    [update]: PostsおよびPostデータクラスの追加

---
+++ src/jsMain/kotlin/model/Post.kt
@@ -0,0 +1,8 @@
+package model
+
+data class Post(
+    var metadata: Metadata?,
+    var fields: Fields,
+    var title: String,
+    var createAt: String
+)
---
+++ src/jsMain/kotlin/model/Posts.kt
@@ -0,0 +1,5 @@
+package model
+
+data class Posts(
+    var posts: List<Post> = emptyList()
+)
