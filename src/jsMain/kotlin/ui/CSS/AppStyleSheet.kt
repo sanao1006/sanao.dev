cd4dd8200a41d0d086af8133ebace683a551b25c
sanao1006 <naosak1006@gmail.com>
2023-05-14 21:42:34 +0900

    [ui]: CSS書いた

--- src/jsMain/kotlin/ui/CSS/AppStyleSheet.kt
+++ src/jsMain/kotlin/ui/CSS/AppStyleSheet.kt
@@ -2,11 +2,30 @@ package ui.CSS
 
 
 import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.keywords.auto
 
 
-object AppStyleSheet: StyleSheet() {
+object AppStyleSheet : StyleSheet() {
+    init {
+        "body, html" style {
+            margin(0.px)
+            height(100.percent)
+        }
+    }
+
     val c_header by style {
         textAlign("center")
         fontSize(2.em)
+        padding(32.px, 0.px)
     }
+
+    val l_content by style {
+        maxWidth(800.px)
+        margin(0.px, auto as CSSNumeric)
+        "article" style {
+            padding(6.px, 0.px)
+        }
+    }
+
+
 }
