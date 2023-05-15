c5f823127d2d971316718d865237a611a2baef9b
sanao1006 <naosak1006@gmail.com>
2023-05-15 21:54:44 +0900

    [refactor]: リファクタリングした (#12)

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,12 +1,22 @@
+import androidx.compose.runtime.Composable
+import app.softwork.routingcompose.HashRouter
 import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.renderComposable
 import ui.CSS.AppStyleSheet
 import ui.Content
+import ui.Header
 
 fun main() {
     renderComposable(rootElementId = "root") {
         Style(AppStyleSheet)
-        ui.Header()
+        MyWebSite()
+    }
+}
+
+@Composable
+fun MyWebSite() {
+    Header()
+    HashRouter("/") {
         Content()
     }
 }
