686ba4d21688ed2f3ce646a4f48e064429b7cc07
sanao1006 <naosak1006@gmail.com>
2023-05-17 16:57:55 +0900

    [refactor]: AppStyleSheetから不要なプロパティ削除 (#17)

--- src/jsMain/kotlin/ui/CSS/AppStyleSheet.kt
+++ src/jsMain/kotlin/ui/CSS/AppStyleSheet.kt
@@ -2,7 +2,6 @@ package ui.CSS
 
 
 import org.jetbrains.compose.web.css.*
-import org.jetbrains.compose.web.css.keywords.auto
 
 
 object AppStyleSheet : StyleSheet() {
@@ -12,27 +11,4 @@ object AppStyleSheet : StyleSheet() {
             height(100.percent)
         }
     }
-
-    val c_header by style {
-        textAlign("center")
-        fontSize(2.em)
-        padding(32.px, 0.px)
-        "a" style {
-            textDecoration("none")
-            color(Color.black)
-            fontFamily("serif")
-            fontWeight("bold")
-        }
-    }
-
-    val l_content by style {
-        maxWidth(800.px)
-        margin(0.px, auto as CSSNumeric)
-        padding(0.px, 10.px, 0.px, 10.px)
-        "article" style {
-            padding(8.px, 0.px)
-        }
-    }
-
-
 }
