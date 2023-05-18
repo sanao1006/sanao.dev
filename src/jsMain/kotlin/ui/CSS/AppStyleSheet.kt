51900b0d2e59db0d5273c67ca353084fe0fcaa59
sanao1006 <naosak1006@gmail.com>
2023-05-18 21:26:39 +0900

    [ui]: サイト全体のCSS (#27)

--- src/jsMain/kotlin/ui/CSS/AppStyleSheet.kt
+++ src/jsMain/kotlin/ui/CSS/AppStyleSheet.kt
@@ -1,6 +1,7 @@
 package ui.CSS
 
 
+import androidx.compose.ui.graphics.Color
 import org.jetbrains.compose.web.css.*
 
 
@@ -9,6 +10,15 @@ object AppStyleSheet : StyleSheet() {
         "body, html" style {
             margin(0.px)
             height(100.percent)
+            backgroundColor(Color("#383838"))
+            color(Color("#fff"))
+            fontFamily("Georgia,Cambria","Times,serif")
+            fontSize(16.8.px)
+            fontWeight(500)
+        }
+        "a" style {
+            color(Color("#f39800"))
+            textDecoration("none")
         }
     }
 }
