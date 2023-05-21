3437bed07e79e1ff88d14e2feadeb51ae33fcf97
sanao1006 <naosak1006@gmail.com>
2023-05-21 23:42:03 +0900

    [fix]: 動的なOGPの設定中止 (#32)

--- src/jsMain/kotlin/ui/PostDetail.kt
+++ src/jsMain/kotlin/ui/PostDetail.kt
@@ -1,53 +1,17 @@
 package ui
 
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import app.softwork.routingcompose.RouteBuilder
-import app.softwork.routingcompose.Routing
-import kotlinx.browser.document
-import model.Meta
 import model.Post
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.H1
 import org.jetbrains.compose.web.dom.Text
 import org.w3c.dom.*
 import ui.CSS.ContentStyleSheet
-import util.OriginalMetaTag
 
-@Routing
 @Composable
-fun RouteBuilder.PostDetail(post: Post, meta: Meta) {
+fun PostDetail(post: Post) {
     SideEffect {
         Prism.highlightAll()
-
-        var head = document.getElementsByTagName("head")[0]!!
-        head.innerHTML = OriginalMetaTag.meta
-        meta.ogtype = "article"
-        meta.ogtitle = post.title
-        meta.ogdesc = post.fields.description ?: ""
-        val ogpMap: Map<String, String> = mapOf(
-            "twitter:card" to "summary",
-            "twitter:site" to "@sanao1006",
-            "og:site_name" to "sanao.dev",
-            "og:type" to "article",
-            "og:title" to "${post.title}",
-            "og:description" to "${post.fields.description ?: ""}",
-            "og:url" to "${decodeURIComponent(js("location.href") as String)}",
-            "og:image" to "https://www.sanao.dev/cardimage.jpg"
-        )
-        ogpMap.forEach { (key, value) ->
-            val metatag = document.createElement("meta")
-            metatag.setAttribute(
-                if (key.contains("twitter")) {
-                    "name"
-                } else {
-                    "property"
-                }, key
-            )
-            metatag.setAttribute("content", value)
-            head.appendChild(metatag)
-        }
-
     }
 
 
--- src/jsMain/kotlin/ui/PostList.kt
+++ src/jsMain/kotlin/ui/PostList.kt
@@ -4,53 +4,21 @@ import androidx.compose.runtime.*
 import app.softwork.routingcompose.NavLink
 import app.softwork.routingcompose.RouteBuilder
 import app.softwork.routingcompose.Routing
-import kotlinx.browser.document
-import model.Meta
 import model.Posts
 import org.jetbrains.compose.web.dom.Article
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Text
-import org.w3c.dom.get
 import ui.CSS.ContentStyleSheet
-import util.OriginalMetaTag
 
 @Routing
 @Composable
 fun RouteBuilder.PostList(contents: Posts) {
     Div(attrs = { classes(ContentStyleSheet.l_content) }) {
-        var head = document.getElementsByTagName("head")[0]!!
-        val meta = Meta
-        val ogpMap: Map<String, String> = mapOf(
-            "twitter:card" to "summary",
-            "twitter:site" to "@sanao1006",
-            "og:site_name" to "sanao.dev",
-            "og:type" to "website",
-            "og:title" to "sanao.dev",
-            "og:description" to "sanao's home",
-            "og:url" to "${decodeURIComponent(js("location.href") as String)}",
-            "og:image" to "https://www.sanao.dev/cardimage.jpg"
-        )
-        SideEffect {
-            head.innerHTML = OriginalMetaTag.meta
-            ogpMap.forEach { (key, value) ->
-            val metatag = document.createElement("meta")
-            metatag.setAttribute(
-                if (key.contains("twitter")) {
-                    "name"
-                } else {
-                    "property"
-                }, key
-            )
-            metatag.setAttribute("content", value)
-            head.appendChild(metatag)
-        }
-
-        }
         for (post in contents.posts) {
             route("post") {
                 string {
                     if (post.fields.slug == decodeURIComponent(it)) {
-                        PostDetail(post = post, meta = meta)
+                        PostDetail(post = post)
                     }
                 }
             }
