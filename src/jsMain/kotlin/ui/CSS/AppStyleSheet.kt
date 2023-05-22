3153791afa1c2edccafebda68597ab395b06f415
sanao1006 <naosak1006@gmail.com>
2023-05-22 23:20:39 +0900

    [WIP]: テーマ色変えボタン機能 (#46)

--- src/jsMain/kotlin/ui/CSS/AppStyleSheet.kt
+++ src/jsMain/kotlin/ui/CSS/AppStyleSheet.kt
@@ -1,11 +1,17 @@
 package ui.CSS
 
-
-import androidx.compose.ui.graphics.Color
+import kotlinx.browser.document
+import kotlinx.browser.window
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import org.jetbrains.compose.web.css.*
-
+import org.w3c.dom.asList
+import org.w3c.dom.get
 
 object AppStyleSheet : StyleSheet() {
+    val themeColor = window.matchMedia("(prefers-color-scheme: dark)").matches
+    private val _isDark = MutableStateFlow(themeColor)
+    val isDark: StateFlow<Boolean> = _isDark
     init {
         "body, html" style {
             margin(0.px)
@@ -16,23 +22,31 @@ object AppStyleSheet : StyleSheet() {
         }
         "a" style {
             textDecoration("none")
+            color(Color("#f39800"))
         }
-        media("prefers-color-scheme", StylePropertyValue("dark")){
-            "body,html" style {
-                backgroundColor(Color("#383838"))
-                color(Color("#fff"))
+    }
+
+    fun setColor(){
+        _isDark.value = !_isDark.value
+        val body = document.getElementsByTagName("body")
+        val atag = document.getElementsByTagName("a")
+        if(_isDark.value){
+        body[0]!!.classList.remove("darkTheme_body")
+            body[0]!!.classList.add("lightTheme_body")
+            atag.asList().forEach {
+                if(it.classList.contains("darkTheme_a")){
+                it.classList.remove("darkTheme_a")
+                }
+                it.classList.add("lightTheme_a")
             }
-            "a" style {
-                color(Color("#f39800"))
-            }
-        }
-        media("prefers-color-scheme", StylePropertyValue("light")){
-            "body,html" style {
-                backgroundColor(Color("#f1f1f1"))
-                color(Color("#333333"))
-            }
-            "a" style {
-                color(Color("#1a0dab"))
+        }else{
+            body[0]!!.classList.remove("lightTheme_body")
+            body[0]!!.classList.add("darkTheme_body")
+            atag.asList().forEach {
+                if(it.classList.contains("lightTheme_a")){
+                it.classList.remove("lightTheme_a")
+                }
+                it.classList.add("darkTheme_a")
             }
         }
     }
