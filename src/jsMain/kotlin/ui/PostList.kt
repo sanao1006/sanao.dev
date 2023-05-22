459bfbd08cb85a3ee44414e9f10859c2f43fa270
sanao1006 <naosak1006@gmail.com>
2023-05-22 18:24:20 +0900

    [refactor]: refactoring (#42)

--- src/jsMain/kotlin/ui/PostList.kt
+++ src/jsMain/kotlin/ui/PostList.kt
@@ -1,9 +1,13 @@
 package ui
 
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import api.getAllPosts
 import app.softwork.routingcompose.NavLink
 import app.softwork.routingcompose.RouteBuilder
 import app.softwork.routingcompose.Routing
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import model.Posts
 import org.jetbrains.compose.web.dom.Article
 import org.jetbrains.compose.web.dom.Div
@@ -12,7 +16,13 @@ import ui.CSS.ContentStyleSheet
 
 @Routing
 @Composable
-fun RouteBuilder.PostList(contents: Posts) {
+fun RouteBuilder.PostList() {
+    var contents by rememberSaveable { mutableStateOf(Posts()) }
+    LaunchedEffect(Unit) {
+        withContext(Dispatchers.Main) {
+            contents = getAllPosts()
+        }
+    }
     Div(attrs = { classes(ContentStyleSheet.l_content) }) {
         for (post in contents.posts) {
             route("post") {
@@ -23,12 +33,11 @@ fun RouteBuilder.PostList(contents: Posts) {
                 }
             }
             noMatch {
-
                 Article {
                     Div {
                         Text(post.createAt.take(10))
                     }
-                    NavLink(to = "/post/${post.fields.slug}") {
+                    NavLink(to = "post/${post.fields.slug}") {
                         Text(post.title)
                     }
                 }
