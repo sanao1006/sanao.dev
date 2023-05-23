4bcf9cb4079b3f0f583ded59a11f6546bacc3845
sanao1006 <naosak1006@gmail.com>
2023-05-23 14:06:40 +0900

    [refactor]: refactoring  (#46)

--- src/jsMain/kotlin/ui/CSS/AppStyleSheet.kt
+++ src/jsMain/kotlin/ui/CSS/AppStyleSheet.kt
@@ -1,7 +1,6 @@
 package ui.CSS
 
 import kotlinx.browser.document
-import kotlinx.browser.window
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import org.jetbrains.compose.web.css.*
@@ -9,14 +8,14 @@ import org.w3c.dom.asList
 import org.w3c.dom.get
 
 object AppStyleSheet : StyleSheet() {
-    val themeColor = window.matchMedia("(prefers-color-scheme: dark)").matches
-    private val _isDark = MutableStateFlow(themeColor)
+    private val _isDark = MutableStateFlow(true)
     val isDark: StateFlow<Boolean> = _isDark
+
     init {
         "body, html" style {
             margin(0.px)
             height(100.percent)
-            fontFamily("Georgia,Cambria","Times,serif")
+            fontFamily("Georgia,Cambria", "Times,serif")
             fontSize(16.8.px)
             fontWeight(500)
         }
@@ -26,28 +25,28 @@ object AppStyleSheet : StyleSheet() {
         }
     }
 
-    fun setColor(){
-        _isDark.value = !_isDark.value
-        val body = document.getElementsByTagName("body")
-        val atag = document.getElementsByTagName("a")
-        if(_isDark.value){
-        body[0]!!.classList.remove("darkTheme_body")
-            body[0]!!.classList.add("lightTheme_body")
-            atag.asList().forEach {
-                if(it.classList.contains("darkTheme_a")){
-                it.classList.remove("darkTheme_a")
+    fun changeThemeColor() {
+        val body = document.getElementsByTagName("body")[0]!!
+        val aTags = document.getElementsByTagName("a")
+        body.classList.remove(document.body!!.className)
+
+        val bodyClass = if (_isDark.value) "lightTheme_body" else "darkTheme_body"
+        body.classList.add(bodyClass)
+
+        val themeClass = if (_isDark.value) "lightTheme_a" else "darkTheme_a"
+        aTags.asList().forEach {
+            it.classList.replace(themeClass,
+                when (themeClass) {
+                    "lightTheme_a" -> {
+                        "darkTheme_a"
+                    }
+                    else -> {
+                        "lightTheme_a"
+                    }
                 }
-                it.classList.add("lightTheme_a")
-            }
-        }else{
-            body[0]!!.classList.remove("lightTheme_body")
-            body[0]!!.classList.add("darkTheme_body")
-            atag.asList().forEach {
-                if(it.classList.contains("lightTheme_a")){
-                it.classList.remove("lightTheme_a")
-                }
-                it.classList.add("darkTheme_a")
-            }
+            )
         }
+        console.log(_isDark.value)
+        _isDark.value = !_isDark.value
     }
 }
