598bc5d9a9507ce7036f551b3e0ac9a3f1b4a81d
sanao1006 <naosak1006@gmail.com>
2023-05-25 21:49:09 +0900

    [ui]: ProfileのCSS実装 (#55)

---
+++ src/jsMain/kotlin/ui/CSS/ProfileStyleSheet.kt
@@ -0,0 +1,33 @@
+package ui.CSS
+
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.keywords.auto
+
+object ProfileStyleSheet : StyleSheet(AppStyleSheet) {
+    val l_profile by style {
+        paddingLeft(12.px)
+        paddingRight(12.px)
+        "h2" style {
+            paddingBottom(4.px)
+        }
+    }
+    val c_profile_icon by style {
+        display(DisplayStyle.Flex)
+        flexDirection(FlexDirection.Column)
+        alignItems(AlignItems.Center)
+        "img" style {
+            borderRadius(80.px)
+            height(auto)
+            maxWidth(100.px)
+            media(mediaMaxWidth(640.px)) {
+                self style {
+                    width(80.px)
+                }
+            }
+        }
+        "span" style {
+            marginTop(10.px)
+            fontSize(19.4.px)
+        }
+    }
+}
