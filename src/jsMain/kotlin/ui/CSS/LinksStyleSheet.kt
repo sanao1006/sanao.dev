d4cefc31a843417f236a7c0c81b868b879754029
sanao1006 <naosak1006@gmail.com>
2023-05-29 22:53:43 +0900

    [UI]: リンクのセクション作成した (#57)

---
+++ src/jsMain/kotlin/ui/CSS/LinksStyleSheet.kt
@@ -0,0 +1,23 @@
+package ui.CSS
+
+import org.jetbrains.compose.web.css.*
+
+object LinksStyleSheet : StyleSheet(ContentStyleSheet) {
+    val c_links by style {
+        "li" style {
+            padding(4.px,0.px)
+        }
+    }
+
+    val c_link_color_dark by style {
+        "li a" style {
+            color(Color("#f39800"))
+        }
+    }
+
+    val c_link_color_light by style {
+        "li a" style {
+            color(Color("#1a0dab"))
+        }
+    }
+}
---
+++ src/jsMain/kotlin/ui/Links.kt
@@ -0,0 +1,35 @@
+package ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import org.jetbrains.compose.web.css.StyleSheet
+import org.jetbrains.compose.web.dom.*
+import ui.CSS.ContentStyleSheet
+import ui.CSS.LinksStyleSheet
+
+@Composable
+fun Links(isDark: State<Boolean>) {
+    Section(attrs = { classes(ContentStyleSheet.l_content) }) {
+        StyleSheet(LinksStyleSheet)
+        H2 { Text("Social") }
+        Div {
+            Ul(attrs = {
+                classes(
+                    LinksStyleSheet.c_links,
+                    if (isDark.value) {
+                        LinksStyleSheet.c_link_color_dark
+                    } else {
+                        LinksStyleSheet.c_link_color_light
+                    }
+                )
+            }){
+                    Li { A(href = "https://github.com/sanao1006") { Text("Github") } }
+                    Li { A(href = "https://blog.sanao.dev") { Text("Blog") } }
+                    Li { A(href = "https://misskey.systems/@sanao") { Text("Misskey.systems") } }
+                    Li { A(href = "https://misskey.dev/@sanao") { Text("Misskey.dev") } }
+                    Li { A(href = "https://twitter.com/sanao1006") { Text("Twitter (ほぼROM専)") } }
+                    Li { A(href = "https://scrapbox.io/sanasana/") { Text("scrapbox") } }
+            }
+        }
+    }
+}
