ae68dec4ad5002b232d047b7fa03d3bf4583d9e7
sanao1006 <naosak1006@gmail.com>
2023-06-04 20:48:09 +0900

    [fix]: Headerをfixfix

--- src/jsMain/kotlin/ui/Header.kt
+++ src/jsMain/kotlin/ui/Header.kt
@@ -4,10 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import app.softwork.routingcompose.NavLink
 import org.jetbrains.compose.web.css.Style
-import org.jetbrains.compose.web.dom.Div
-import org.jetbrains.compose.web.dom.H1
-import org.jetbrains.compose.web.dom.Header
-import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.dom.*
 import ui.styles.HeaderStyleSheet
 
 @Composable
@@ -19,41 +16,7 @@ fun Header(isDark: State<Boolean>) {
                 HeaderStyleSheet.c_header,
             )
         }) {
-            H1 { Text("sanao.dev") }
-            Div {
-                NavLink(
-                    to = "/",
-                    attrs = {
-                        classes(
-                            if (isDark.value) {
-                                HeaderStyleSheet.c_headerColor_dark
-                            } else {
-                                HeaderStyleSheet.c_headerColor_light
-                            }
-                        )
-                    }
-                ) {
-                    Text("Home")
-                }
-                NavLink(
-                    to = "/posts",
-                    attrs = {
-                        classes(
-                            if (isDark.value) {
-                                HeaderStyleSheet.c_headerColor_dark
-                            } else {
-                                HeaderStyleSheet.c_headerColor_light
-                            }
-                        )
-                    }
-                ) {
-                    Text("Post")
-                }
-
-
-            }
+            Text("sanao.dev")
         }
     }
-
-
 }
--- src/jsMain/kotlin/ui/styles/HeaderStyleSheet.kt
+++ src/jsMain/kotlin/ui/styles/HeaderStyleSheet.kt
@@ -7,27 +7,16 @@ object HeaderStyleSheet : StyleSheet() {
         textAlign("center")
         padding(32.px, 0.px)
         flexGrow(0)
-        fontSize(1.2.em)
-        "a" style {
-            textDecoration("underline")
-            fontWeight("bold")
-        }
-        "h1" style {
-            margin(0.px)
-        }
-        "div" style {
-            fontSize(24.px)
-            display(DisplayStyle.Flex)
-            justifyContent(JustifyContent.Center)
-            gap(24.px)
-            marginTop(10.px)
-        }
+        fontSize(2.em)
+        textDecoration("underline")
+        fontWeight("bold")
+
     }
-    val c_headerColor_light by style{
+    val c_headerColor_light by style {
         color(Color("#202020"))
     }
 
-    val c_headerColor_dark by style{
+    val c_headerColor_dark by style {
         color(Color("#ffffff"))
     }
 }
