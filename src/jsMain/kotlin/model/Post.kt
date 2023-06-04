f2b8a7d485d6601d84c664e7768d186091699e2a
sanao1006 <naosak1006@gmail.com>
2023-06-04 20:55:54 +0900

    [fix]: API関連のファイルを除去

--- src/jsMain/kotlin/model/Post.kt
+++
@@ -1,8 +0,0 @@
-package model
-
-data class Post(
-    var metadata: Metadata?,
-    var fields: Fields,
-    var title: String,
-    var createAt: String,
-)
--- src/jsMain/kotlin/model/Posts.kt
+++
@@ -1,5 +0,0 @@
-package model
-
-data class Posts(
-    var posts: List<Post> = emptyList()
-)
