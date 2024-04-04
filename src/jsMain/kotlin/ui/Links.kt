9fa222b93696a5a6b7028ad9be7047b5ca46aa87
sanao <naosak1006@gmail.com>
2024-04-04 10:44:41 +0900

    Update Links.kt

--- src/jsMain/kotlin/ui/Links.kt
+++ src/jsMain/kotlin/ui/Links.kt
@@ -22,9 +22,10 @@ fun Links(isDark: State<Boolean>) {
                     }
                 )
             }) {
-                Li { A(href = "https://misskey.sanao.dev/@sanao") { Text("さなおすきー") } }
-                Li { A(href = "https://twitter.com/sanao1006") { Text("X (Twitter)") } }
                 Li { A(href = "https://github.com/sanao1006") { Text("Github") } }
+                Li { A(href = "https://www.reddit.com/user/sanao1006/") { Text("Reddit") } }
+                Li { A(href = "https://misskey.sanao.dev/@sanao") { Text("Misskey") } }
+                Li { A(href = "https://twitter.com/sanao1006") { Text("X (Twitter)") } }
                 Li { A(href = "https://scrapbox.io/sanasana/") { Text("Scrapbox") } }
                 Li { A(href = "mailto:jinsei@sanao.dev") { Text("Mail") } }
                 Li { A(href = "https://bsky.app/profile/bsky.sanao.dev") { Text("Bluesky") } }
